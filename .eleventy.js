348e719da104edcca5daf0f77cde35562504273c
Kintiru <Kintiru@gmail.com>
2023-04-09 04:36:47 +0000

    Update eleventy config to reflect current directory

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
     return {
         dir: {
-            includes: "includes",
-            layouts: "layouts"
-        }
+            includes: "layouts",
+        },
     }
 };
